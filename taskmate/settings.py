e872a04f64f72e5099ff3fd8a7fd1a94b3051760
Zodiark619 <seonghyejin33@gmail.com>
2022-02-12 21:53:56 +0700

    heroku deploying

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import environ
 import os
@@ -137,3 +137,6 @@ CRISPY_TEMPLATE_PACK='bootstrap4'
 LOGIN_REDIRECT_URL='todolist'
 
 LOGIN_URL='login'
+
+
+django_heroku.settings(locals())
