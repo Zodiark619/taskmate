ef55bbf1e37712a6ae6e75f69d4c23aa8faccddd
Zodiark619 <seonghyejin33@gmail.com>
2022-02-12 22:41:14 +0700

    heroku deployz

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     'static',
@@ -141,4 +141,3 @@ LOGIN_URL='login'
 
 django_heroku.settings(locals())
 
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
